c9821d4d74dc6bd1e3f78bdb4d6ca67f59badceb
Bunbohue <105864582+bumbowei@users.noreply.github.com>
2025-10-18 07:17:13 +0700

    16

--- Assets/Scripts/UI/UI_InGame.cs
+++ Assets/Scripts/UI/UI_InGame.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI_InGame : MonoBehaviour
 {
@@ -7,6 +8,9 @@ public class UI_InGame : MonoBehaviour
     public UI_FadeEffect fadeEffect { get; private set; }
     [SerializeField] private TextMeshProUGUI timerText;
     [SerializeField] private TextMeshProUGUI fruitText;
+
+    [SerializeField] private GameObject pauseUI;
+    private bool isPaused;
 
     private void Awake()
     {
@@ -23,6 +27,36 @@ public class UI_InGame : MonoBehaviour
     {
         fruitText.text = collectedFruits + "/" + totalFruits;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseButton();
+        }
+    }
+
+    public void PauseButton()
+    {
+        if(isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+            pauseUI.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+            pauseUI.SetActive(true);
+        }
+    }
+
+    public void GoToMainMenuButtom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void UpdateTimerUI(float timer)
     {
         timerText.text = timer.ToString("00" + " s");
